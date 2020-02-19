41ffc5e491a9b8283d732f7448c739ddda6fdcf2
Henry Le <35796147+HenryLeNgocThang@users.noreply.github.com>
2020-02-19 22:30:41 +0100

    rev

--- virus-simulator/MainWindow.xaml.cs
+++ virus-simulator/MainWindow.xaml.cs
@@ -13,9 +13,8 @@ namespace virus_simulator
     public partial class MainWindow : Window
     {
         List<string> names = new List<string>() { "AF", "AO", "AL", "AE", "AR", "AM", "AU", "AT", "AZ", "BI", "BE", "BJ", "BF", "BD", "BG", "BA", "BY", "BZ", "BO", "BR", "BN", "BT", "BW", "CF", "CA", "CH", "CL", "CN", "CI", "CM", "CD", "CG", "CO", "CR", "CU", "CZ", "DE", "DJ", "DK", "DO", "DZ", "EC", "EG", "ER", "EE", "ET", "FI", "FJ", "GA", "GB", "GE", "GH", "GN", "GM", "GW", "GQ", "GR", "GL", "GT", "GY", "HN", "HR", "HT", "HU", "ID", "IN", "IE", "IR", "IQ", "IS", "IL", "IT", "JM", "JO", "JP", "KZ", "KE", "KG", "KH", "KR", "XK", "KW", "LA", "LB", "LR", "LY", "LK", "LS", "LT", "LU", "LV", "MA", "MD", "MG", "MX", "MK", "ML", "MM", "ME", "MN", "MZ", "MR", "MW", "MY", "NA", "NE", "NG", "NI", "NL", "NO", "NP", "NZ", "OM", "PK", "PA", "PE", "PH", "PG", "PL", "KP", "PT", "PY", "PS", "QA", "RO", "RU", "RW", "EH", "SA", "SD", "SS", "SN", "SL", "SV", "RS", "SR", "SK", "SI", "SE", "SZ", "SY", "TD", "TG", "TH", "TJ", "TM", "TL", "TN", "TR", "TW", "TZ", "UG", "UA", "UY", "US", "UZ", "VE", "VN", "VU", "YE", "ZA", "ZM", "ZW", "SO", "GF", "FR", "ES", "AW", "AI", "AD", "AG", "BS", "BM", "BB", "KM", "CV", "KY", "DM", "FK", "FO", "GD", "HK", "KN", "LC", "LI", "MF", "MV", "MT", "MS", "MU", "NC", "NR", "PN", "PR", "PF", "SG", "SB", "ST", "SX", "SC", "TC", "TO", "TT", "VC", "VG", "VI", "CY", "RE", "YT", "MQ", "GP", "CW", "IC" };
-        //List<Path> codes = new List<Path>() { AF, AO, AL, AE, AR, AM, AU, AT, AZ, BI, BE, BJ, BF, BD, BG, BA, BY, BZ, BO, BR, BN, BT, BW, CF, CA, CH, CL, CN, CI, CM, CD, CG, CO, CR, CU, CZ, DE, DJ, DK, DO, DZ, EC, EG, ER, EE, ET, FI, FJ, GA, GB, GE, GH, GN, GM, GW, GQ, GR, GL, GT, GY, HN, HR, HT, HU, ID, IN, IE, IR, IQ, IS, IL, IT, JM, JO, JP, KZ, KE, KG, KH, KR, XK, KW, LA, LB, LR, LY, LK, LS, LT, LU, LV, MA, MD, MG, MX, MK, ML, MM, ME, MN, MZ, MR, MW, MY, NA, NE, NG, NI, NL, NO, NP, NZ, OM, PK, PA, PE, PH, PG, PL, KP, PT, PY, PS, QA, RO, RU, RW, EH, SA, SD, SS, SN, SL, SV, RS, SR, SK, SI, SE, SZ, SY, TD, TG, TH, TJ, TM, TL, TN, TR, TW, TZ, UG, UA, UY, US, UZ, VE, VN, VU, YE, ZA, ZM, ZW, SO, GF, FR, ES, AW, AI, AD, AG, BS, BM, BB, KM, CV, KY, DM, FK, FO, GD, HK, KN, LC, LI, MF, MV, MT, MS, MU, NC, NR, PN, PR, PF, SG, SB, ST, SX, SC, TC, TO, TT, VC, VG, VI, CY, RE, YT, MQ, GP, CW, IC };
-        //List<countries> countryCode = new List<countries>();
-        List<Path> countryCode = new List<Path>();
+        List<Path> codes = new List<Path>();
+        SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(255, 50, 0, 0));
 
         public MainWindow()
         {
@@ -23,11 +22,26 @@ namespace virus_simulator
 
             foreach (string name in names)
             {
-                Path myTextBlock = (Path)this.FindName(name);
-                countryCode.Add(myTextBlock);
+                Path path = (Path)FindName(name);
+                codes.Add(path);
             }
 
-            Path path = new Path();
+            // Fill all countries with color
+            foreach (Path code in codes)
+            {
+                if (code != null)
+                {
+                    code.Fill = brush;
+                }
+            }
+
+            // Change brush color
+            brush = new SolidColorBrush(Color.FromArgb(255, 127, 255, 0));
+
+            // Deutschland ist an der 37 stelle
+            codes[36].Fill = brush;
+
+            lvCountries.ItemsSource = codes;
         }
     }
 }
